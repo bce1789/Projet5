38879fe4df7c7c6c0f4a6e6739934740c131e395
Benoit Coste <bce.1789@gmail.com>
2020-06-16 13:00:28 +0200

    redefine slideshow file in POO and apply this for each slideshow on the homepage

--- assets/js/slideShow.js
+++ assets/js/slideShow.js
@@ -1,22 +1,12 @@
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 2000);
-
-function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
-
-/* class SlideShow {
-    constructor() {
-        this.slides = document.querySelectorAll('.slide');
+class SlideShow {
+    constructor(container) {
+        this.slides = document.querySelectorAll(container + ' .slide');
         this.currentSlide = 0;
-        this.slideInterval = null;
+        this.slideInterval = setInterval(this.nextSlide.bind(this), 3000);
     }
     nextSlide() {
         this.slides[this.currentSlide].className = 'slide';
         this.currentSlide = (this.currentSlide + 1) % this.slides.length;
         this.slides[this.currentSlide].className = 'slide showing';
     }
-} */
+}
