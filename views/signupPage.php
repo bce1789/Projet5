080f3d4d4c3b90b02e453e877549f35ce860c5aa
Benoit Coste <bce.1789@gmail.com>
2020-06-29 09:26:13 +0200

    update css minors rules

--- views/signupPage.php
+++ views/signupPage.php
@@ -6,9 +6,9 @@ ob_start(); ?>
         <div class="contact-clean">
             <form action="/P5_benoit_coste/index.php?action=signup" method="post">
                 <h2 class="text-center">Enregistrement</h2>
-                <div class="form-group"><input class="form-control" type="text" name="adminName" placeholder="Nom"></div>
-                <div class="form-group"><input class="form-control" type="password" name="adminPassword" placeholder="Password"></div>
-                <div class="form-group"><input class="form-control" type="password" name="adminPassword_confirm" placeholder="Password-confirm"></div>
+                <div class="form-group"><input class="form-control" type="text" name="userName" placeholder="Nom"></div>
+                <div class="form-group"><input class="form-control" type="password" name="userPassword" placeholder="Password"></div>
+                <div class="form-group"><input class="form-control" type="password" name="userPassword_confirm" placeholder="Password-confirm"></div>
                 <div class="form-group"><button class="btn btn-primary" type="submit">S'enregister</button></div>
             </form>
         </div>
